ec35f66ca78fb5694c2a84cb6f885409319b416c
toinhao1 <toinhao89@gmail.com>
2019-11-17 10:23:16 +0100

    Basic routing set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 
-function App() {
+const App = () => {
   return (
     <div >
       <HomePage />
