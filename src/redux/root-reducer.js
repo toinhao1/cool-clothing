f12a0632da5c076ef4491190719ca327b95257bb
toinhao1 <toinhao89@gmail.com>
2019-12-17 16:00:52 +0100

    persisiting the cart

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+export const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer
 });
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,25 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
-import rootReducer from './root-reducer';
+import { rootReducer } from './root-reducer';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: ['cart']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [logger];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-	rootReducer,
+export const store = createStore(
+	persistedReducer,
 	composeEnhancers(applyMiddleware(...middlewares))
 );
 
-export default store;
+export const persistor = persistStore(store);
